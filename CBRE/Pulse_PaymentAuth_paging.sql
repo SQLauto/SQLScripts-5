a79af7fcefbd0606ec371045d081d06557b4a308
Carter <Phil.Carter@cbre.com>
2019-10-21 15:46:27 +1100

    re-worked number of pages and random page selection

--- CBRE/Pulse_PaymentAuth_paging.sql
+++ CBRE/Pulse_PaymentAuth_paging.sql
@@ -1,12 +1,10 @@
 --DECLARE @entityID char(6) = '110076' -- Braddon Central (the small one)
 DECLARE @entityID char(6) = '131310' -- Eastland Shopping Centre (the big one)
-
 -- user settable/selectable, on change refresh list of pages
-DECLARE @rowsToFetch int = 100;
-SELECT @rowsToFetch as [Number of rows to fetch];
+DECLARE @linesToFetch int = 10;
 
 -- total lines
-DECLARE @totalRows int = (
+DECLARE @totalLines int = (
 SELECT COUNT(*)
 FROM [MRI_AUNZ].[dbo].[HIST] h WITH (nolock)
 WHERE h.[CASHTYPE] = 'OP'
@@ -16,22 +14,92 @@ WHERE h.[CASHTYPE] = 'OP'
        AND h.[ENTITYID] = @entityID
        AND h.[TAXITEM] = 'N'
 );
-SELECT @totalRows as [Total rows available];
 
--- build list of available pages, show as clickable items ??
+-- build list of available pages
+DECLARE @availPages varchar(50) = '< unset >';
 WITH lv0 AS (SELECT 0 g UNION ALL SELECT 0)
-       ,lv1 AS (SELECT 0 g FROM lv0 a CROSS JOIN lv0 b) -- 4 rows
-       ,lv2 AS (SELECT 0 g FROM lv1 a CROSS JOIN lv1 b) -- 16 rows
-       ,lv3 AS (SELECT 0 g FROM lv2 a CROSS JOIN lv2 b) -- 256 rows
-       ,Tally (n) AS (SELECT 0 UNION ALL SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) FROM lv3
+       ,lv1 AS (SELECT 0 g FROM lv0 a CROSS JOIN lv0 b) -- 4 lines
+       ,lv2 AS (SELECT 0 g FROM lv1 a CROSS JOIN lv1 b) -- 16 lines
+       ,lv3 AS (SELECT 0 g FROM lv2 a CROSS JOIN lv2 b) -- 256 lines
+       ,Tally (n) AS (SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) FROM lv3
 )
-SELECT DISTINCT t.[n] 
+, lineCount AS (
+    SELECT COUNT(*) as [totalLines]
+    FROM [MRI_AUNZ].[dbo].[HIST] h WITH (nolock)
+    WHERE h.[CASHTYPE] = 'OP'
+       AND h.[CHECKPD] IS NULL  
+       AND h.[STATUS] NOT IN ('U','C','W','D','V','T') 
+       AND ((h.[ATAXID] IS NOT NULL AND (h.[TRANSFERITEM] = 'N' OR h.[TRANSFERITEM] IS NULL)) OR h.[ATAXID] IS NULL) 
+       AND h.[ENTITYID] = @entityID
+       AND h.[TAXITEM] = 'N'
+)
+-- list of pages for drop-down list
+SELECT DISTINCT t.[n] as [Page]
 FROM Tally t
-WHERE t.[n] <= CASE WHEN @totalRows <= @rowsToFetch THEN 0 ELSE (@totalRows / @rowsToFetch)-1 END
+    CROSS JOIN [lineCount] lc
+WHERE t.[n] <= CASE WHEN lc.[totalLines] <= @linesToFetch THEN 1 ELSE (lc.[totalLines] / @linesToFetch) END
 
--- which page number we want to show
+-- -- build comma seperated list ??
+-- SELECT @availPages = STUFF(( 
+-- 	SELECT  ',' + CAST(x.[n] as varchar(10))
+--     FROM ( 
+-- 			SELECT DISTINCT t.[n] 
+-- 			FROM Tally t
+--                 CROSS JOIN [lineCount] lc
+-- 			WHERE t.[n] <= CASE WHEN lc.[totalLines] <= @linesToFetch THEN 1 ELSE (lc.[totalLines] / @linesToFetch) END
+-- 		) x
+-- 		FOR XML PATH('')
+-- 	), 1, 1, '') 
+
+
+-- just for example and testing, randomly select which page number we want to show
 DECLARE @pageNumber int = 0;
+WITH lv0 AS (SELECT 0 g UNION ALL SELECT 0)
+       ,lv1 AS (SELECT 0 g FROM lv0 a CROSS JOIN lv0 b) -- 4 lines
+       ,lv2 AS (SELECT 0 g FROM lv1 a CROSS JOIN lv1 b) -- 16 lines
+       ,lv3 AS (SELECT 0 g FROM lv2 a CROSS JOIN lv2 b) -- 256 lines
+       ,Tally (n) AS (SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) FROM lv3
+)
+, lineCount AS (
+    SELECT COUNT(*) as [totalLines]
+    FROM [MRI_AUNZ].[dbo].[HIST] h WITH (nolock)
+    WHERE h.[CASHTYPE] = 'OP'
+       AND h.[CHECKPD] IS NULL  
+       AND h.[STATUS] NOT IN ('U','C','W','D','V','T') 
+       AND ((h.[ATAXID] IS NOT NULL AND (h.[TRANSFERITEM] = 'N' OR h.[TRANSFERITEM] IS NULL)) OR h.[ATAXID] IS NULL) 
+       AND h.[ENTITYID] = @entityID
+       AND h.[TAXITEM] = 'N'
+)
+, numList as (
+    -- list of pages for drop-down list
+    SELECT DISTINCT t.[n] as [Page]
+    FROM Tally t
+        CROSS JOIN [lineCount] lc
+    WHERE t.[n] <= CASE WHEN lc.[totalLines] <= @linesToFetch THEN 1 ELSE (lc.[totalLines] / @linesToFetch) END
+    -- -- split comma seperated string
+    -- SELECT n - Len(REPLACE(LEFT(@availPages,n),',','')) + 1 AS [Page]
+    --     ,CAST(Substring(@availPages,n,Charindex(',',@availPages + ',',n) - n) AS INT) AS [Element]
+    -- FROM (SELECT @availPages AS arr) AS a
+    --     JOIN Tally ON n <= Len(@availPages)
+    --         AND Substring(',' + @availPages,n,1) = ','
+)
+, listWithMax as (
+    SELECT [Page], MAX([Page]) OVER() as [maxPage]
+    FROM [numList]
+)
+-- need to have subtract 1 for zero based offset
+SELECT @pageNumber = ISNULL((CEILING(RAND()*[maxPage])-1),0)
+FROM [listWithMax];
 
+-- what did we calculate
+SELECT 
+    @totalLines as [Total lines available]
+    , @linesToFetch as [Number of lines to fetch]
+    , @availPages as [List of available pages for user to click]
+    , @pageNumber as [Selected page to view (zero based)];
+
+
+-- show the lines
 SELECT 
        h.[STATUS], h.[PONUM], ph.[ORDSTAT], v.[VENDNME1], h.[INVOICE], h.[ITEMAMT]
        , h.[ATAXAMT], h.[ACCTNUM], g.[ACCTNAME], i.[INVCDATE], i.[DUEDATE], h.[VENDID]
@@ -48,5 +116,6 @@ WHERE h.[CASHTYPE] = 'OP'
        AND h.[ENTITYID] = @entityID
        AND h.[TAXITEM] = 'N'
 ORDER BY h.[STATUS], h.[VENDID], h.[INVOICE], h.[EXPPED]
-OFFSET (@pageNumber * @rowsToFetch) ROWS FETCH NEXT @rowsToFetch ROWS ONLY;
+OFFSET (@pageNumber * @linesToFetch) ROWS FETCH NEXT @linesToFetch ROWS ONLY;
+
 
